c64e795cff7e781f305b2b382c233f9052e7e5ef
Forykx <prusa.vojtech@gmail.com>
2015-10-05 23:38:25 +0200

    hawkularAddJDBCDriversTest

--- src/main/java/org/qe/hawkular/element/HawkularAppServerPageConstants.java
+++ src/main/java/org/qe/hawkular/element/HawkularAppServerPageConstants.java
@@ -8,9 +8,10 @@ public class HawkularAppServerPageConstants {
     public static final By localWildfyServerLink = By.xpath("//a[contains(@href,'/hawkular-ui/app/app-details/') and contains(@href,'~Local/jvm')]");
     public static final By appServerJvmtabLocator = By.xpath("//*[text()='JVM Status']");
     public static final By appServerDeploymentsLink = By.linkText("Deployments");
+    public static final By appServerDatasourcestabLink = By.linkText("Datasources");
     public static final By appServerDeploymentstabLocator = By.xpath("//*[text()='Deployment Status']");
     public static final By appServerWarFileLocator = By.xpath("//*[text()='hawkular-accounts.war']");
     public static final By appServerRHALocator = By.xpath("//li[@class='dropdown']//a[contains(text(),'Red Hat Access')]");
     public static final By appServerRHASearchLocator = By.xpath("//a[@href='/hawkular-ui/access/search']");
-   
+    
 }
