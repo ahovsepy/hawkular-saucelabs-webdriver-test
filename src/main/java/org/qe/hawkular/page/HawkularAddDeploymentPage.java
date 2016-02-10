53fbd96ffc1a01fb21bdc151766406d6077172bc
sunil kondkar <skondkar@redhat.com>
2016-02-10 15:43:51 +0530

    Added test hawkularDeploymentAlertTest and refactored HawkularAddDeploymentTest.java.

--- src/main/java/org/qe/hawkular/page/HawkularAddDeploymentPage.java
+++ src/main/java/org/qe/hawkular/page/HawkularAddDeploymentPage.java
@@ -7,46 +7,61 @@ import org.qe.hawkular.util.HawkularUtils;
 
 public class HawkularAddDeploymentPage {
 
-	public final WebDriver driver;
+    public final WebDriver driver;
 
     public HawkularAddDeploymentPage(WebDriver driver) {
         this.driver = driver;
     }
-	
-	 By addDeploymentTab = HawkularAddDeploymentConstants.addDeploymentTab;
-	 By uploadFileTab = HawkularAddDeploymentConstants.uploadFileTab;
-	 String applicationFilePath = HawkularAddDeploymentConstants.applicationFilePath;
-	 String overviewApplicationFilePath = HawkularAddDeploymentConstants.overviewApplicationFilePath;
-	 By applicationFilePathNextButton = HawkularAddDeploymentConstants.applicationFilePathNextButton;
-	 By addDeploymentNextButton = HawkularAddDeploymentConstants.addDeploymentNextButton;
-	 By verifyDeploymentSuccess = HawkularAddDeploymentConstants.verifyDeploymentSuccess;
-	 
-	 
-	 public void addDeploymentTab() {
-	        HawkularUtils util = new HawkularUtils(driver);
-	        util.navigateTo(addDeploymentTab);
-	        util.waitForElementPresent(uploadFileTab);
-	    }
-	 public void uploadApplicationFile() {
-	        HawkularUtils util = new HawkularUtils(driver);
-	        util.sendKeysTo(uploadFileTab, applicationFilePath);
-	        util.navigateTo(applicationFilePathNextButton);
-	 }
-	 
-	 public void overviewUploadApplicationFile() {
-	        HawkularUtils util = new HawkularUtils(driver);
-	        util.waitForElementPresent(uploadFileTab);
-	        util.sendKeysTo(uploadFileTab, overviewApplicationFilePath);
-	        util.navigateTo(applicationFilePathNextButton);
-	 }
-	 
-	  public void deployApplication() {
-	        HawkularUtils util = new HawkularUtils(driver);
-	        util.waitForElementPresent(addDeploymentNextButton);
-	        util.navigateTo(addDeploymentNextButton);
-	    }
-	  public void verfiySuccess() {
-			HawkularUtils utils = new HawkularUtils(driver);
-			utils.waitForElementPresent(verifyDeploymentSuccess);
-	  }
+
+    By addDeploymentTab = HawkularAddDeploymentConstants.addDeploymentTab;
+    By uploadFileTab = HawkularAddDeploymentConstants.uploadFileTab;
+    String applicationFilePath = HawkularAddDeploymentConstants.applicationFilePath;
+    String overviewApplicationFilePath = HawkularAddDeploymentConstants.overviewApplicationFilePath;
+    By applicationFilePathNextButton = HawkularAddDeploymentConstants.applicationFilePathNextButton;
+    By addDeploymentNextButton = HawkularAddDeploymentConstants.addDeploymentNextButton;
+    By verifyDeploymentSuccess = HawkularAddDeploymentConstants.verifyDeploymentSuccess;
+
+    public void addDeploymentTab() {
+        HawkularUtils util = new HawkularUtils(driver);
+        util.navigateTo(addDeploymentTab);
+        util.waitForElementPresent(uploadFileTab);
+    }
+
+    public void uploadApplicationFile() {
+        HawkularUtils util = new HawkularUtils(driver);
+        util.sendKeysTo(uploadFileTab, applicationFilePath);
+        util.navigateTo(applicationFilePathNextButton);
+    }
+
+    public void overviewUploadApplicationFile() {
+        HawkularUtils util = new HawkularUtils(driver);
+        util.waitForElementPresent(uploadFileTab);
+        util.sendKeysTo(uploadFileTab, overviewApplicationFilePath);
+        util.navigateTo(applicationFilePathNextButton);
+    }
+
+    public void deployApplication() {
+        HawkularUtils util = new HawkularUtils(driver);
+        util.waitForElementPresent(addDeploymentNextButton);
+        util.navigateTo(addDeploymentNextButton);
+    }
+
+    public void verfiySuccess() {
+        HawkularUtils utils = new HawkularUtils(driver);
+        utils.waitForElementPresent(verifyDeploymentSuccess);
+    }
+
+    public void verifyDeploymentAlertDetails() {
+        HawkularUtils utils = new HawkularUtils(driver);
+        utils.waitForElementPresent(HawkularAddDeploymentConstants.deploymentAlertText);
+    }
+
+    public void verifyDefinitionsLink() {
+        HawkularUtils utils = new HawkularUtils(driver);
+        utils.waitForElementPresent(HawkularAddDeploymentConstants.deploymentAlertDefinitionsLink);
+        driver.findElement(
+                HawkularAddDeploymentConstants.deploymentAlertDefinitionsLink)
+                .click();
+    }
+
 }
--- src/test/java/org/qe/hawkular/tests/HawkularAddDeploymentTest.java
+++ src/test/java/org/qe/hawkular/tests/HawkularAddDeploymentTest.java
@@ -1,14 +1,21 @@
 package org.qe.hawkular.tests;
 
+import java.net.MalformedURLException;
+
 import org.openqa.selenium.WebDriver;
 import org.qe.hawkular.driver.HawkularSeleniumLocalWebDriver;
 import org.qe.hawkular.driver.HawkularSeleniumWebDriver;
 import org.qe.hawkular.element.HawkularRegistrationPageConstants;
 import org.qe.hawkular.page.HawkularAddDeploymentPage;
+import org.qe.hawkular.page.HawkularAlertFilterPage;
+import org.qe.hawkular.page.HawkularAlertsSettingsPage;
 import org.qe.hawkular.page.HawkularAppServerPage;
 import org.qe.hawkular.page.HawkularConsoleAddUrlPage;
 import org.qe.hawkular.page.HawkularLoginPage;
+import org.qe.hawkular.page.HawkularManageOrganizationsPage;
+import org.qe.hawkular.util.HawkularUtils;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -17,68 +24,81 @@ import org.testng.annotations.Test;
  */
 
 public class HawkularAddDeploymentTest extends HawkularSeleniumLocalWebDriver {
-	WebDriver driver = null;
+    WebDriver driver = null;
+    HawkularLoginPage loginPage = null;
+    HawkularConsoleAddUrlPage url = null;
+    HawkularAppServerPage appserverPage = null;
+    HawkularAddDeploymentPage adddeployment = null;
+    HawkularUtils util = null;
+    HawkularAlertFilterPage alertFilter = null;
 
-    public WebDriver hawkularLoginToDeployments() throws Exception {
+    @BeforeMethod
+    public void createBrowserInstance() throws MalformedURLException {
+
         driver = createLocalDriver();
-
         driver.get(HawkularSeleniumWebDriver.hawkularUrl);
         _logger.info(driver.getTitle());
 
-        HawkularLoginPage loginPage = new HawkularLoginPage(driver);
+        loginPage = new HawkularLoginPage(driver);
+        appserverPage = new HawkularAppServerPage(driver);
+        adddeployment = new HawkularAddDeploymentPage(driver);
+        url = new HawkularConsoleAddUrlPage(driver);
+        util = new HawkularUtils(driver);
+        alertFilter = new HawkularAlertFilterPage(driver);
 
         loginPage = new HawkularLoginPage(driver);
         loginPage.loginAs(HawkularRegistrationPageConstants.username2,
                 HawkularRegistrationPageConstants.password2);
-
-        HawkularConsoleAddUrlPage clickAppServer = new HawkularConsoleAddUrlPage(
-                driver);
-        clickAppServer.navigateToAppServersMenu();
-
-        HawkularAppServerPage selectAppServer = new HawkularAppServerPage(
-                driver);
-
-        selectAppServer.verifyLocalAppServerExists();
-        selectAppServer.navigateToLocalAppServer();
-        selectAppServer.navigateToDeploymentsTab();
-        
-
-        return driver;
+        appserverPage.verifyLocalAppServerExists();
+        appserverPage.navigateToLocalAppServer();
+        appserverPage.navigateToDeploymentsTab();
     }
-    
+
     @AfterMethod
-	public void closeSession() {
-		driver.quit();
-	}
+    public void closeSession() {
+        driver.quit();
+    }
 
     @Test
     public void hawkularAddDeploymentTest() throws Exception {
-        driver = hawkularLoginToDeployments();
-        HawkularAddDeploymentPage adddeployment = new HawkularAddDeploymentPage(
-                driver);
+        _logger.info("Deploy a war file in app server deployment tab");
         adddeployment.addDeploymentTab();
         adddeployment.uploadApplicationFile();
         adddeployment.deployApplication();
         adddeployment.verfiySuccess();
-        // TODO check successful upload after fix of https://issues.jboss.org/browse/HAWKULAR-678
- //       driver.quit();
+        // TODO check successful upload after fix of
+        // https://issues.jboss.org/browse/HAWKULAR-678
+        // driver.quit();
     }
-    
+
     /**
      * Test to verify adding a deployment from 'OverView' tab of the app server.
      */
+    
     @Test
     public void hawkularOverviewAddDeploymentTest() throws Exception {
-        driver = hawkularLoginToDeployments();
-        HawkularAppServerPage appserverPage = new HawkularAppServerPage(
-                driver);
+        _logger.info("Deploy a war file from app server overview tab");
         appserverPage.navigateToOverviewTab();
         appserverPage.navigateToOverviewAddDeployment();
-        HawkularAddDeploymentPage adddeployment = new HawkularAddDeploymentPage(
-                driver);
         adddeployment.overviewUploadApplicationFile();
         adddeployment.deployApplication();
         adddeployment.verfiySuccess();
-        
+
     }
+
+    /**
+     * Test to verify if the alert settings for failed deployment alert exists.
+     */
+    @Test
+    public void hawkularDeploymentAlertTest() throws Exception {
+        _logger.info("Verifying alert settings/details for failed deployment alerts");
+        adddeployment.verifyDeploymentAlertDetails();
+        adddeployment.verifyDefinitionsLink();
+        alertFilter.filter("Deployment Failure");
+        alertFilter.verifyFailedDeploymentAlertExists();
+        alertFilter.navigateToViewDetails();
+        alertFilter.verifyFailedDeploymentAlertDetails();
+
+    }
+
 }
